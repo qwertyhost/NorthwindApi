cfdd05f0abcaf617eb9e0e9e9e5f6f13be2c94b9
Pavitar-Singh <112693477+Pavitar-Singh@users.noreply.github.com>
2022-10-05 15:13:25 +0100

    update

--- src/main/java/com/sparta/northwindapi/controller/NorthwindController.java
+++ src/main/java/com/sparta/northwindapi/controller/NorthwindController.java
@@ -1,12 +1,34 @@
 package com.sparta.northwindapi.controller;
 
+import com.sparta.northwindapi.entity.Employee;
+import com.sparta.northwindapi.entity.Employeeterritory;
+import com.sparta.northwindapi.entity.Territory;
+import com.sparta.northwindapi.repo.EmployeeRepository;
+import com.sparta.northwindapi.repo.EmployeeterritoryRepository;
+import com.sparta.northwindapi.repo.RegionRepository;
+import com.sparta.northwindapi.repo.TerritoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class NorthwindController {
 
+    @Autowired
+    private EmployeeRepository employeeRepo;
+
+    @Autowired
+    private EmployeeterritoryRepository employeeterritoryRepo;
+
+    @Autowired
+    private RegionRepository regionRepo;
+
+    @Autowired
+    private TerritoryRepository territoryRepo;
+
     @GetMapping("/Employee")
+
+    @GetMapping
 
 
 }
