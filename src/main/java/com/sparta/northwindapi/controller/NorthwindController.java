5358f5173200422c2689bd4abb48f10df50a03af
Pavitar-Singh <112693477+Pavitar-Singh@users.noreply.github.com>
2022-10-06 12:31:08 +0100

    update

--- src/main/java/com/sparta/northwindapi/controller/NorthwindController.java
+++ src/main/java/com/sparta/northwindapi/controller/NorthwindController.java
@@ -1,5 +1,6 @@
 package com.sparta.northwindapi.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sparta.northwindapi.entity.Employee;
 import com.sparta.northwindapi.entity.Region;
@@ -10,11 +11,14 @@ import com.sparta.northwindapi.repo.RegionRepository;
 import com.sparta.northwindapi.repo.TerritoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class NorthwindController {
@@ -54,7 +58,26 @@ public class NorthwindController {
         return employeeterritoryRepo.findAll().stream().filter(c-> c.getEmployeeID().getId() ==id).map(c->c.getTerritoryID()).toList();
     }
 
-
-
-
+    @GetMapping("/territory/{id}")
+    public ResponseEntity<String> getTerritory(@PathVariable String id) {
+        Optional<Territory> foundTerritory = territoryRepo.findById(id);
+        mapper = new ObjectMapper();
+        headers = new HttpHeaders();
+        headers.add("content-type","application/json");
+        ResponseEntity<String> result = null;
+        if (foundTerritory.isPresent()) {
+            try {
+                result = new ResponseEntity<>(
+                        mapper.writeValueAsString(foundTerritory.get()), headers,
+                        HttpStatus.OK);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        else {
+            result = new ResponseEntity<>("{\"message\":\"Territory not found\"}",
+                    headers, HttpStatus.OK);
+        }
+        return result;
+    }
 }
